a1ddf9e879fc3785cad563a1c7caa7a9e2f01c54
class02 <class02@192.168.0.62>
2020-06-01 18:14:45 +0900

    200601 첫 날 기본세팅

---
+++ cls/src/main/java/com/increpas/cls/Test01.java
@@ -0,0 +1,13 @@
+package com.increpas.cls;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test01 {
+
+	@RequestMapping("/test/t01.cls")
+	public String test01() {
+		return "test/test01";
+	}
+}
---
+++ cls/src/main/java/com/increpas/cls/vo/MemberVO.java
@@ -0,0 +1,97 @@
+package com.increpas.cls.vo;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+
+public class MemberVO {
+	private int mno, ano;
+	private String name, id, pw, mail, gen, tel, sdate, avatar;
+	private Date jDate;
+	private Time jTime;
+	public int getMno() {
+		return mno;
+	}
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getGen() {
+		return gen;
+	}
+	public void setGen(String gen) {
+		this.gen = gen;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getSdate() {
+		return sdate;
+	}
+	public void setSdate() {
+		SimpleDateFormat form1 = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat form2 = new SimpleDateFormat("HH:mm:ss");
+		this.sdate = form1.format(jDate) + " " + form2.format(jTime);
+	}
+	public void setSdate(String sdate) {
+		this.sdate = sdate;
+	}
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public Date getjDate() {
+		return jDate;
+	}
+	public void setjDate(Date jDate) {
+		this.jDate = jDate;
+	}
+	public Time getjTime() {
+		return jTime;
+	}
+	public void setjTime(Time jTime) {
+		this.jTime = jTime;
+	}
+	
+	@Override
+	public String toString() {
+		return "MemberVO [mno=" + mno + ", ano=" + ano + ", name=" + name + ", id=" + id + ", pw=" + pw + ", mail="
+				+ mail + ", gen=" + gen + ", tel=" + tel + ", avatar=" + avatar + ", jDate=" + jDate + ", jTime="
+				+ jTime + "]";
+	}
+	
+}

5c2d6d392852c8a8a95504835e0311302e5aac35
class02 <class02@192.168.0.62>
2020-06-18 18:44:31 +0900

    200618 동적질의(trim) 실습

---
+++ cls/src/main/java/com/increpas/cls/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.increpas.cls.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.increpas.cls.dao.MemberDAO;
+import com.increpas.cls.vo.MemberVO;
+
+/**
+ * 이 클래스는 회원정보와 관련된 작업을 처리할 서비스적인 기능을 담당할 클래스
+ * @author	이명환
+ * @since	2020.06.18
+ * @version	v.1.0
+ *
+ */
+public class MemberService {
+	@Autowired
+	MemberDAO mDAO;
+	
+	public int editMember(MemberVO mVO) {
+		int cnt = 0;
+		// 할일
+		// 받은 데이터로 데이터베이스 작업을 해주고 결과받아서 반환해준다
+		cnt = mDAO.editMember(mVO);
+		return cnt;
+	}
+}
